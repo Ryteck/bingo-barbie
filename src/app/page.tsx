7b85d6f13fbcddfb1cc1828695a781fee0168d99
Ryan Gomes Martins <ryteckgm@gmail.com>
2024-08-07 20:56:05 -0300

    primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+const Page: FC = () => (
+	<main className="w-screen h-screen flex items-center justify-center">
+		<h1 className="text-4xl">Bingo da Barbie</h1>
+	</main>
+);
+
+export default Page;
